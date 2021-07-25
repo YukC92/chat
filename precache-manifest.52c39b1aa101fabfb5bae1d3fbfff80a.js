b1911059731d207d53a81144d48a6d7772322054
yuk fai chan <yukfaichan92@gmail.com>
2021-07-25 15:48:40 -0700

    Updates

---
+++ precache-manifest.52c39b1aa101fabfb5bae1d3fbfff80a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29aef4a306a5d7073c9787b581264433",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "9884ed2ae00382f47fe3",
+    "url": "/chat/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "3fd74742c75f803f3fbe",
+    "url": "/chat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "9884ed2ae00382f47fe3",
+    "url": "/chat/static/js/2.dd61cd1f.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/chat/static/js/2.dd61cd1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fd74742c75f803f3fbe",
+    "url": "/chat/static/js/main.9b7a548c.chunk.js"
+  },
+  {
+    "revision": "4c860faa05c7142c8a22",
+    "url": "/chat/static/js/runtime-main.4852f890.js"
+  }
+]);
