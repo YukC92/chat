5883b6d661ee1bc8a3390f7af74f7a28eaefe392
yuk fai chan <yukfaichan92@gmail.com>
2021-07-26 17:05:05 -0700

    minor changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
     return (
         <div id='login-page'>
             <div id='login-card'>
-                <h2>Welcome to Unichat!</h2>
+                <h2>Welcome to Chat!</h2>
 
                 <div 
                     className="login-button google"
